b5ad139d5ae3cd7f02e39fe7295ba232bc5048f3
ryuwc <wom05169@gmail.com>
2022-10-24 22:24:32 +0900

    JS DOM, event 실습

--- test.js
+++ test.js
@@ -1,21 +1,7 @@
-function calculateCircleArea(r) {
-  return Math.PI * r * r;
+function isAnimal(name) {
+  const animals = ['고양이', '개', '거북이', '너구리'];
+  return name in animals;
 }
 
-const area = calculateCircleArea(4);
-console.log(area); // 50.26548245743669
-
-
-const savedFile = {
-  name: 'profile',
-  extension: 'jpg',
-  size: 29930
-}
-// const {name, extension, size} = savedFile
-
-function fileSummary({name, extension, size}) {
-    console.log(`The file ${name}.${extension} is size of ${size} bytes.`)
-}
-fileSummary(savedFile);
-
-
+console.log(isAnimal('개')); // true
+console.log(isAnimal('노트북')); // false
